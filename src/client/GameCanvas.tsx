c4af63639dc491d7da34228f121d9ffdfd3fb669
M Nef <howtoeatpineapples@gmail.com>
2020-03-30 16:21:13 +0800

    added canvas item

--- src/client/GameCanvas.tsx
+++ src/client/GameCanvas.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { makeStyles, Paper } from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+    root: {
+        marginTop: '5em',
+        display: 'flex',
+        justifyContent: 'center'
+    },
+    canvas: {
+        display: 'flex-item',
+        width: '25em',
+        height: '25em',
+        background: 'red'
+	}
+}));
 
 const GameCanvas: React.FC<{}> = () => {
+   
+    const classes = useStyles(); 
+   
     return (
-        <> </>
+        <div className={classes.root}>
+            <Paper elevation={3} className={classes.canvas} />
+        </div>
     ); 
 }
 
