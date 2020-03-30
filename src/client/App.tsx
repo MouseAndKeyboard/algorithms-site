742299eec5d2ac74d51b638fd3183e7cd8a80e48
M Nef <howtoeatpineapples@gmail.com>
2020-03-30 15:02:36 +0800

    changed app to functional component

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,36 +1,19 @@
-import * as React from 'react';
+import React from 'react'
+import {AppBar, Toolbar} from '@material-ui/core'
 
-class App extends React.Component<IAppProps, IAppState> {
-	constructor(props: IAppProps) {
-		super(props);
-		this.state = {
-			name: null
-		};
-	}
 
-	async componentDidMount() {
-		try {
-			let r = await fetch('/api/hello');
-			let name = await r.json();
-			this.setState({ name });
-		} catch (error) {
-			console.log(error);
-		}
-	}
-
-	render() {
-		return (
-			<main className="container my-5">
-				<h1 className="text-primary text-center">Hello {this.state.name}!</h1>
-			</main>
-		);
-	}
-}
-
-export interface IAppProps {}
-
-export interface IAppState {
-	name: string;
+const App = () => {
+	
+	return (
+		<>
+		<AppBar>
+			<Toolbar>
+				<>
+			</Toolbar>
+		</AppBar>
+		test
+		</>
+	);
 }
 
 export default App;
