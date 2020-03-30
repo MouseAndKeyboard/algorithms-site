9b8b51217e3e01ef40c69ce0a37cdf4af0ee130a
M Nef <howtoeatpineapples@gmail.com>
2020-03-30 16:06:21 +0800

    created app component

---
+++ src/client/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MainHeader from './MainHeader';
+
+const App: React.FC<{}> = () => {
+    return (
+        <MainHeader Title='Algorithms'/>
+    );
+}
+
+export default App;
