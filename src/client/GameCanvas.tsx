3930e2d49fecde5272fe9ff3aa0d8ca287f59d30
M Nef <howtoeatpineapples@gmail.com>
2020-03-30 16:36:57 +0800

    customisable styles

--- src/client/GameCanvas.tsx
+++ src/client/GameCanvas.tsx
@@ -1,27 +1,33 @@
 import React from 'react'
-import { makeStyles, Paper } from '@material-ui/core';
+import { makeStyles, Paper, Grid } from '@material-ui/core';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles(theme => ({
     root: {
         marginTop: '5em',
         display: 'flex',
-        justifyContent: 'center'
+        justifyContent: 'center',
     },
-    canvas: {
+    canvas: props => ({
         display: 'flex-item',
         width: '25em',
         height: '25em',
-        background: 'red'
-	}
+        background: props.color,
+	})
 }));
 
 const GameCanvas: React.FC<{}> = () => {
    
-    const classes = useStyles(); 
+    const classes = useStyles({color: '#292E37'}); 
    
     return (
         <div className={classes.root}>
-            <Paper elevation={3} className={classes.canvas} />
+            <Paper elevation={3} className={classes.canvas}>
+
+                <Grid container>
+                    
+                </Grid>
+            
+            </Paper>
         </div>
     ); 
 }
