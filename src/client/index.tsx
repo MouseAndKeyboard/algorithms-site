c4313b3a9c1f9a351f329648be2df068bf538b48
M Nef <howtoeatpineapples@gmail.com>
2020-03-30 16:07:04 +0800

    added app to main render

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import MainHeader from './MainHeader';
+import App from './App';
 
-render(<MainHeader Title='Algorithms'/>, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
