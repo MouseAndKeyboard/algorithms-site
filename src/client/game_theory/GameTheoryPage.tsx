7dc83dafdc33ffa751c7581fc4de46db4cbe2e8e
M Nef <howtoeatpineapples@gmail.com>
2020-04-16 00:50:12 +0800

    added folder organisation

---
+++ src/client/game_theory/GameTheoryPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+
+const Home: React.FC<{}> = () => {
+    return (
+        <>
+            <h1>Hello world</h1>
+        </>
+    );  
+}
+
+export default Home;
